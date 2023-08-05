c83bc41b715be38c1176ee5267a050b2d1546436
Amansharma70 <131703368+Amansharma70@users.noreply.github.com>
2023-08-05 13:44:32 +0530

    Add files via upload

---
+++ myfunction.java
@@ -0,0 +1,18 @@
+class Myfunction
+{
+void show(int a[])
+{
+int t=a[];
+for(int i=0;i<a.length;i++)
+{
+System.out.println(a[i]);
+}
+}
+public static void main(String...a1)
+{
+Myfunction m1=new Myfunction();
+int a[]=new int[5];
+a[]={10,20,30,40};
+m1.show(a);
+}
+}
